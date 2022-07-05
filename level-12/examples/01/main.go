e98b6b7427aa215997984d9f31189f9d2000eaed
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2022-07-05 19:53:12 -0300

    Added an example for level 12

---
+++ level-12/examples/01/main.go
@@ -0,0 +1,17 @@
+// Este é um pacote de exemplo
+package jose
+
+import "fmt"
+
+// Esta função exibe o nome do usuário
+func PrintName() {
+	fmt.Println("My name is Jose")
+}
+
+// Esta função exibe a idade do usuário
+func PrintAge() {
+	fmt.Println("I am 29 years old")
+}
+
+// Suba para o github.com
+// Acesse godoc.org/{url-do-repo}
